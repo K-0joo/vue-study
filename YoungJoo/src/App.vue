71f38d0a09cb712d6b08f6789f70e0efdbae3b19
K-0joo <tyty4420@naver.com>
2022-05-21 20:40:49 +0900

    2week

--- YoungJoo/src/App.vue
+++ YoungJoo/src/App.vue
@@ -1,56 +1,90 @@
 <template>
-  <!--  
-    <div v-show="toggle">true</div>
-    <div v-show="!toggle">false</div> 
-    -->
-  <div v-if="toggle">true</div>
-  <div v-else>false</div>
-  
-  <button @click="onToggle">Toggle</button>
-  <div class = "container">
-
+  <div class="container">
+    <h2>To-Do List</h2>
+     <input
+        class="form-control"
+        type="text" 
+        v-model="searchText"
+        placeholder="Search"
+      >
+      <hr />
+    <TodoSimpleForm @add-todo="addTodo" />
+ 
+    <div v-if="!filterdTodos.length">
+      There is nothing to display
+    </div>
+    <TodoList 
+      :todos="filterdTodos" 
+      @toggle-todo="toggleTodo" 
+      @deleteTodo="deleteTodo"
+      />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, computed} from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
+
 export default {
-  setup(){
-      const toggle = ref(false);
-      const todo = ref('');
-      const todos = ref([
-        {id: 1, subject: '휴대폰 사기'},
-        {id: 2, subject: '장보기'},
-      ]);
+  components : {
+    TodoSimpleForm,
+    TodoList,
+},
 
-      const onSubmit = () => {
-        //e.preventDefault(); //refresh하는 속성을 안하게 만듬.
-        todos.value.push({
-          id: Date.now(), //TimeStamp
-          subject: todo.value //제목    
-        });
-      };
+  setup() {
+    const todos = ref([]);
+    const todoStyle ={
+      textDecoration: 'line-through',
+      color: 'gray'
+    };
 
-      const onToggle =() =>{
-        toggle.value = !toggle.value;
-      };
+    const addTodo = (todo) => {
+      console.log(todo);
+      //자식이 보낸 데이터 확인
+      todos.value.push(todo);
+    };
 
+    const toggleTodo = (index) => {
+      console.log(todos.value[index]); //변경되기 전
+      todos.value[index].completed = !todos.value[index].completed; // 토글 : true -> false, false -> true로 변경해주는 소스
+      console.log(todos.value[index]); //변경된 후
+    };
 
-      return {
-        todo,
-        onSubmit,
-        todos,
-        toggle,
-        onToggle,
-      };
+    const deleteTodo = (index) => {
+      todos.value.splice(index, 1);
+    };
+
+    const searchText = ref('');
+    const filterdTodos = computed(() => {
+
+      //필터가 된 Array를 넘김
+      if(searchText.value) {
+        return todos.value.filter(todo => {
+          return todo.subject.includes(searchText.value);
+        });   
+      }
+
+      // 아무것도 없을 시에는 원래 있던 todos를 출력해준다.
+      return todos.value;
+    });
+ 
+    return {
+      todos,
+      addTodo,
+      todoStyle,
+      toggleTodo,
+      deleteTodo,
+      searchText,
+      filterdTodos,
+    };
   }
 }
 </script>
 
 <style>
-
-  .name{
-    color: red;
+  .todo {
+    color: gray;
+    text-decoration: line-through;
   }
-
 </style>
